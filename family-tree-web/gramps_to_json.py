baf6a9f94c451ef33d59cc42eb3d486b94c2f903
Frank Kolden <elgost@gmail.com>
2026-09-24 01:29:12 +0200

    First  json

--- family-tree-web/gramps_to_json.py
+++ family-tree-web/gramps_to_json.py
@@ -11,38 +11,49 @@ def main():
     ap.add_argument("input", help="Gramps .gramps XML export")
     ap.add_argument("-o","--output",default="data/family-tree.json")
     a=ap.parse_args()
-    root=ET.parse(a.input)#.getroot()
+    root=ET.parse(a.input).getroot()
     people={}
-    pl=root.findall(".//person")
-    print(f"Found {pl}")
+    #pl=root.findall(".//{http://gramps-project.org/xml/1.7.1/}person")
+    #print(f"Found {pl}")
     print(f"Root {root}")
-    for p in root.findall(".//person"):
-        pid=p.get("id")
+    for p in root.findall(".//{http://gramps-project.org/xml/1.7.1/}person"):
+        pid=p.get("handle")
         if not pid: continue
-        n=p.find("./name")
-        first=txt(n,"./first") if n is not None else ""
-        last=txt(n,"./surname") if n is not None else ""
+        n=p.find("./{http://gramps-project.org/xml/1.7.1/}name")
+        first=txt(n,"./{http://gramps-project.org/xml/1.7.1/}first") if n is not None else ""
+        last=txt(n,"./{http://gramps-project.org/xml/1.7.1/}surname") if n is not None else ""
         people[pid]={"id":pid,"name":" ".join(x for x in (first,last) if x),
-                     "gender":p.get("gender",""),"birth":"","death":""}
-        # for er in p.findall("./eventref"):
-        #     ev=root.find(".//event[@id='"+str(er.get("hlink"))+"']")
-        #     if ev is None: continue
-        #     typ=txt(ev,"./type").lower()
-        #     d=ev.find("./date")
-        #     val=d.get("val","") if d is not None else ""
-        #     if typ=="birth": people[pid]["birth"]=val
-        #     if typ=="death": people[pid]["death"]=val
+                     "gender":txt(p,"./{http://gramps-project.org/xml/1.7.1/}gender"),"birth":"","birthplace":"","death":"", "deathplace":""}
+        for er in p.findall("./{http://gramps-project.org/xml/1.7.1/}eventref"):
+            
+            ev=root.find(".//{http://gramps-project.org/xml/1.7.1/}event[@handle='"+str(er.get("hlink"))+"']")
+            
+            if ev is None: continue
+            typ=txt(ev,"./{http://gramps-project.org/xml/1.7.1/}type").lower()
+            d=ev.find("./{http://gramps-project.org/xml/1.7.1/}dateval")
+            print(typ, d)
+            val=d.get("val","") if d is not None else ""
+
+            pla=ev.find("./place")
+            placelink = root.find(".//{http://gramps-project.org/xml/1.7.1/}placeobj[@handle='" +str(pla.get("hlink"))+"']")
+            placename = txt(placelink,"./{http://gramps-project.org/xml/1.7.1/}name") if placelink is not None else ""
+            if typ=="birth": people[pid]["birth"]=val; people[pid]["birthplace"]=placename
+            if typ=="death": people[pid]["death"]=val; people[pid]["deathplace"]=placename
+
 
     families=[]
-    for f in root.findall(".//family"):
-        fid=f.get("id")
+    for f in root.findall(".//{http://gramps-project.org/xml/1.7.1/}family"):
+        fid=f.get("handle")
         if not fid: continue
-        fa=f.find("./father"); mo=f.find("./mother")
+        fa=f.find("./{http://gramps-project.org/xml/1.7.1/}father"); mo=f.find("./{http://gramps-project.org/xml/1.7.1/}mother")
+        children=[]
+        for c in f.findall("./{http://gramps-project.org/xml/1.7.1/}childref"):
+            children.append(c.get("hlink"))
         families.append({
           "id":fid,
           "father":fa.get("hlink") if fa is not None else None,
           "mother":mo.get("hlink") if mo is not None else None,
-          "children":[c.get("hlink") for c in f.findall("./child") if c.get("hlink")]
+          "children":children
         })
     out=Path(a.output); out.parent.mkdir(parents=True,exist_ok=True)
     out.write_text(json.dumps({"people":list(people.values()),"families":families},ensure_ascii=False,indent=2),encoding="utf-8")
